066e22f27f1af7ab8a5b9a7d6d166c8d34e8f80f
JoshuaBeals <joshua@camelcase.co>
2022-04-05 14:54:37 +0100

    Added logos for light and dark mode switch

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,9 @@
+import light from '../assets/img/light.png';
+import dark from '../assets/img/dark.png';
+import qrcode from '../assets/img/qr-code.png';
 import React from 'react';
 import useLocalStorage from 'use-local-storage';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { changeTheme } from '../actions';
 
 const Header = () => {
@@ -8,6 +11,7 @@ const Header = () => {
     const [theme, setTheme] = useLocalStorage('theme');
 
     const dispatch = useDispatch();
+    const themeData = useSelector(state => state.theme);
 
     const switchTheme = () => {
         const newTheme = theme === 'light' ? 'dark' : 'light';
@@ -19,9 +23,14 @@ const Header = () => {
         <nav className="navbar">
             <div className="container">
                 <a className="navbar-brand">
-                  <i className="bi bi-bootstrap"></i>
-                  <button onClick={() => switchTheme()}>Change</button>
+                    <img src={qrcode} alt="Logo" />
+                    <span>QR Chef</span>
                 </a>
+                <div className='d-flex'>
+                    <button className='btn toogle-btn' onClick={() => switchTheme()} data-bs-toggle="tooltip" data-bs-placement="bottom" title="Toogle dark mode">
+                        {themeData == 'light' ? <img src={dark} alt='dark' /> : <img src={light} alt='light' />}
+                    </button>
+                </div>
             </div>
         </nav>
     );
